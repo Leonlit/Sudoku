b45037a511ac478efbcedc686193d200089cac4b
Leon <leonlit123@gmail.com>
2021-04-04 23:10:33 +0800

    sudoku solver done

--- Board.java
+++ Board.java
@@ -1,8 +1,12 @@
 public class Board {
     private int board[][] = new int[9][9];
 
-    public void changeValue (int posX, int posY, int value) {
-        this.board[posX][posY] = value;
+    public Board (int[][] newBoard) {
+        this.board = newBoard;
+    }
+
+    public int[][] getBoard(){ 
+        return this.board;
     }
 
     public void printBoard () {
@@ -21,24 +25,18 @@ public class Board {
         boardLine();
     }
 
-    public int[][] getBoard(){
-        return this.board;
-    }
-
-    public boolean validateBoard (int x, int y) {
-        int[][] grid = this.board;
+    private boolean validateBoard (int x, int y, int board[][]) {
         String temp = "";
         int count = 0, idx = 0;
         for (int index = 0; index < 9; index++) {
             temp += Integer.toString(board[index][y]);  // Horizontal
             temp += Integer.toString(board[x][index]);  // vertical
-            temp += Integer.toString(grid[(x/3) * 3 + index/3][(y/3) * 3 + index % 3]);
+            temp += Integer.toString(board[(x/3) * 3 + index/3][(y/3) * 3 + index % 3]); // sub-section
         }
 
-        while ((idx = temp.indexOf(Integer.toString(grid[x][y]), idx)) != -1) {
+        while ((idx = temp.indexOf(Integer.toString(board[x][y]), idx)) != -1) {
             idx++;  count++;
         }
-        System.out.println(temp);
         return count == 3;
     }
 
@@ -47,5 +45,36 @@ public class Board {
             System.out.print("-");
         }
         System.out.println();
+    }
+
+    public boolean solveBoard(int[][] board) {
+        int pos[] = findEmpty(board);
+        int rows = pos[0];
+        int columns = pos[1];
+        if (pos[0] == -1) {
+            return true;
+        }
+        for (int num = 1; num < 10  ;num++) {
+            board[rows][columns] = num;
+            boolean result = this.validateBoard(rows, columns, board);
+            if (result) {
+                if (solveBoard(board)) {
+                    return true;
+                }
+            }
+            board[rows][columns] = 0;
+        }
+        return false;
+    }
+
+    private int[] findEmpty (int[][] board) {
+        for (int rows = 0; rows < 9; rows++) {
+            for (int columns = 0; columns < 9; columns++) {
+                if (board[rows][columns] == 0) {
+                    return new int[] {rows, columns};
+                }
+            }
+        }
+        return new int[] {-1, -1};
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
 class Main {
     public static void main (String args[]){
-        Board test = new Board();
+        int[][] sampleBoard = {
+            {7,8,0,4,0,0,1,2,0},
+            {6,0,0,0,7,5,0,0,9},
+            {0,0,0,6,0,1,0,7,8},
+            {0,0,7,0,4,0,2,6,0},
+            {0,0,1,0,5,0,9,3,0},
+            {9,0,4,0,6,0,0,0,5},
+            {0,7,0,3,0,0,0,1,2},
+            {1,2,0,0,0,7,4,0,0},
+            {0,4,9,2,0,6,0,0,7}
+        };
+        Board test = new Board(sampleBoard);
         test.printBoard();
-        test.validateBoard(1, 4);
+        test.solveBoard(test.getBoard());
+        test.printBoard();
     }
 }
